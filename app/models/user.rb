e6839ffcbc5d68a1248dacff6a4258e9b91cbe4b
Morcik <b.truchel@gmail.com>
2013-11-04 13:26:28 -0800

    basic user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
-  
+  before_save {self.email = email.downcase}
+  validates :name, presence: true 
+  validates :email, presence: true, uniqueness: {case_sensitive: false}  
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,31 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before {@user = User.new(name: "Hello Kitty", email:"hello@kitty.com")}
+  
+  subject {@user}
+
+  it {should respond_to(:name)}
+  it {should respond_to(:email)}
+  it {should be_valid}
+
+  describe "when name is blank" do
+  	before { @user.name =" "}
+  	it { should_not be_valid }
+  end
+
+  describe "when email is blank" do
+  	before { @user.email =" "}
+  	it { should_not be_valid }
+  end
+
+  describe "when email is not unique" do 
+  	before do
+  	 dupped_user = @user.dup
+     dupped_user.email = @user.email.upcase
+     dupped_user.save
+   end
+
+   it {should_not be_valid}
+  end
 end
