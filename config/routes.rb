88ea557121f9e8dd262c1a0988a4786810cdbada
Morcik <b.truchel@gmail.com>
2013-11-06 13:52:32 -0800

    going backwards to go forwards

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 WIMS::Application.routes.draw do
+  match '/about', to: "static_pages#about", via: 'get'
+  match '/contact', to: "static_pages#contact", via: 'get'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'static_pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -12,7 +15,7 @@ WIMS::Application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  resources :users
 
   # Example resource route with options:
   #   resources :products do
